bafdb55ab751a7fcee5728ff99352a355ed6829b
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-05-09 01:20:00 -0700

    Add 3d axis position overlay; use webgl if possible

--- js/silversword.js
+++ js/silversword.js
@@ -103,13 +103,13 @@ function init() {
     // - assume we've got jQuery to hand
     var $container = $('#container');
 
-    renderer = new THREE.CanvasRenderer({
-    });
+    renderer = Detector.webgl ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
     camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
             ASPECT,
             NEAR,
             FAR  );
     scene = new THREE.Scene();
+    // axisScene = new THREE.Scene();
 
     // the camera starts at 0,0,0 so pull it back
     camera.position.set( 10, 10, 20 );
@@ -142,60 +142,78 @@ function init() {
     stats.domElement.style.position = 'relative';
     stats.domElement.style.left = '0px';
     stats.domElement.style.top = '0px';
-    // $container.append( stats.domElement );
+    $container.append( stats.domElement );
 
     // and the camera
     scene.add(camera);
 
-    var dirLight = new THREE.DirectionalLight( 0x555555 );
-    dirLight.position.set( 0, -.5, -.5 ); 
-    scene.add(dirLight);
+    function addLightsTo(scene) {
+        var dirLight = new THREE.DirectionalLight( 0x555555 );
+        dirLight.position.set( 0, -.5, -.5 );
+        scene.add(dirLight);
 
-    var dirLight = new THREE.DirectionalLight( 0x666666 );
-    dirLight.position.set( 0, .5, -.5 ); 
-    scene.add(dirLight);
+        var dirLight = new THREE.DirectionalLight( 0x666666 );
+        dirLight.position.set( 0, .5, -.5 );
+        scene.add(dirLight);
 
-    var dirLight = new THREE.DirectionalLight( 0x555555 );
-    dirLight.position.set( -.8, -.2, .1 ); 
-    scene.add(dirLight);
+        var dirLight = new THREE.DirectionalLight( 0x555555 );
+        dirLight.position.set( -.8, -.2, .1 );
+        scene.add(dirLight);
 
-    var dirLight = new THREE.DirectionalLight( 0x888888 );
-    dirLight.position.set( .1, .2, .9 ); 
-    scene.add(dirLight);
+        var dirLight = new THREE.DirectionalLight( 0x888888 );
+        dirLight.position.set( .1, .2, .9 );
+        scene.add(dirLight);
 
-    scene.add(new THREE.AmbientLight( 0x555555 ));
+        scene.add(new THREE.AmbientLight( 0x555555 ));
+    }
 
-    // (function() {
+    addLightsTo(scene);
+    // addLightsTo(axisScene);
 
-    //     var len = 100;
+    var axis = (function () {
 
-    //     var matR = new THREE.LineBasicMaterial({ color: 0xff0000 });
-    //     var matG = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-    //     var matB = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        function makeThickArrowMesh(color) {
+            var material = new THREE.MeshLambertMaterial({color: color});
 
-    //     var geometry = new THREE.Geometry();
-    //     geometry.vertices.push(new THREE.Vector3());
-    //     geometry.vertices.push(new THREE.Vector3(100, 0, 0));
-    //     var line = new THREE.Line(geometry, matR);
-    //     scene.add(line);
+            var arrowBase = new THREE.CylinderGeometry(.4, .4, 3, 10, 1, true);
+            arrowBase.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 1.5, 0 ) );
 
-    //     var geometry = new THREE.Geometry();
-    //     geometry.vertices.push(new THREE.Vector3());
-    //     geometry.vertices.push(new THREE.Vector3(0, 100, 0));
-    //     var line = new THREE.Line(geometry, matG);
-    //     scene.add(line);
+            var arrowTip = new THREE.CylinderGeometry(0, .8, 1.5, 10, 1, false);
+            arrowTip.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 3 + 0.75, 0 ) );
 
-    //     var geometry = new THREE.Geometry();
-    //     geometry.vertices.push(new THREE.Vector3());
-    //     geometry.vertices.push(new THREE.Vector3(0, 0, 100));
-    //     var line = new THREE.Line(geometry, matB);
-    //     scene.add(line);
+            THREE.GeometryUtils.merge(arrowBase, arrowTip);
 
-    // })();
+            return new THREE.Mesh(arrowBase, material);
+        }
+
+        var axis = new THREE.Object3D();
+
+        var topArrow = makeThickArrowMesh(0xff0000);
+        axis.add(topArrow);
+
+        var xArrow = makeThickArrowMesh(0xffffff);
+        xArrow.rotation.set( 0, 0, -Math.PI / 2 );
+        axis.add(xArrow);
+
+        var zArrow = makeThickArrowMesh(0xffffff);
+        zArrow.rotation.set( Math.PI / 2, 0, 0 );
+        axis.add(zArrow);
+
+        var ball = new THREE.Mesh(new THREE.SphereGeometry(.6), new THREE.MeshLambertMaterial({color: 0xffffff}));
+        axis.add(ball);
+
+        var scale = 0.01;
+        axis.scale.set( scale, scale, scale );
+
+        return axis;
+    })();
+
+    window.axis = axis;
+    scene.add(axis);
+    // axisScene.add(axis);
 
     window.sphere = undefined;
     window.shouldUpdateUI = true;
-
 
 };
 
@@ -204,12 +222,34 @@ function render() {
     requestAnimationFrame(render);
 
     controls.update();
+    camera.updateMatrix();
+    camera.updateMatrixWorld(true);
 
-    renderer.render(scene, camera);
-    stats.end();
     if(window.shouldUpdateUI) {
         updateUI();
     }
+    updateAxisPosition();
+
+    renderer.render(scene, camera);
+    stats.end();
+}
+
+function updateAxisPosition() {
+
+    var screenX = 55,
+        screenY = 55;
+    var vector = new THREE.Vector3( THREE.Math.mapLinear(screenX, 0, renderer.domElement.width, -1, 1),
+                                    THREE.Math.mapLinear(screenY, 0, renderer.domElement.height, 1, -1),
+                                    0.5 );
+    var projector = new THREE.Projector();
+    projector.unprojectVector( vector, camera );
+
+    var dir = vector.sub( camera.position ).setLength( .5 );
+
+
+    axis.position.copy( camera.position );
+    axis.position.add( dir );
+
 }
 
 init();
