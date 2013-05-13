a801097f584ee253e3d5631b302e9cbc5145316d
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-05-12 18:56:03 -0700

    RBLT ordering; improve readability; prepare for fanlike

--- js/trapezoid.js
+++ js/trapezoid.js
@@ -14,6 +14,15 @@ function lerp(vector, end, amount) {
     );
 }
 
+function projectScalar(a, b) {
+    return a.dot(b) / b.length();
+}
+
+function to3D(v2) {
+    return new THREE.Vector3(v2.x, v2.y, 0);
+}
+
+
 function TrapezoidGeometry(sphere, lonIndex, latIndex) {
     THREE.Geometry.call(this);
 
@@ -86,6 +95,56 @@ function TrapezoidGeometry(sphere, lonIndex, latIndex) {
 
 TrapezoidGeometry.prototype = Object.create( THREE.Geometry.prototype );
 
+// Transform a Face4 into its 2D version
+TrapezoidGeometry.prototype.faceTo2D = function(face) { //should be a Face4
+    var a = this.vertices[face.a],
+        b = this.vertices[face.b],
+        c = this.vertices[face.c],
+        d = this.vertices[face.d];
+
+    //define local 3D coordinate system
+    var x3 = d.clone().sub(a).normalize();
+    var z3 = x3.clone().cross( b.clone().sub(a) ).normalize();
+    var y3 = z3.clone().cross( x3 );
+
+    // Return a specific point in the Face4 into its 2D location
+    function pointToLocal2D(point) {
+        var p = point.clone().sub(a);
+        var xValue = projectScalar(p, x3),
+            yValue = projectScalar(p, y3);
+        return new THREE.Vector2(xValue, yValue);
+    }
+
+    var pointsLocal = [face.a, face.b, face.c, face.d].map(function (idx) {
+        return this.vertices[idx];
+    }.bind(this)).map(pointToLocal2D);
+
+    return pointsLocal;
+}
+
+//return face2D transformed to line up the edge with the end of reference
+TrapezoidGeometry.prototype.faceToTrapezoid = function(face2D, reference) {
+    var oldDC = to3D(reference[2].clone().sub(reference[3]));
+    var origin = to3D(reference[3]);
+    var newAB = to3D(face2D[1].clone().sub(face2D[0]));
+
+
+    var rotAngle = Math.asin( newAB.clone().normalize().cross(oldDC.clone().normalize()).z );
+
+
+    // var transform = new THREE.Matrix4().makeFromPositionEulerScale( origin, new THREE.Vector3(0, 0, rotAngle), "XYZ", new THREE.Vector3(1,1,1) );
+
+    return face2D.map(function (pt) {
+        var txPt3 = to3D(pt);
+        // txPt3.applyMatrix4(transform);
+        txPt3.applyMatrix4( new THREE.Matrix4().makeRotationZ( rotAngle ) );
+        txPt3.add( origin );
+
+        return new THREE.Vector2(txPt3.x, txPt3.y);
+    });
+}
+
+
 /*
  *  Returns a 10 element Array of Vector2D's.
  *
@@ -122,72 +181,48 @@ TrapezoidGeometry.prototype.unroll = function() {
     //
     // trapezoid-wide 2D plane is just the local 2D plane of the first face
 
-    // Transform a Face4 into its 2D version
-    var faceInLocal2D = function(face) { //should be a Face4
-        var a = this.vertices[face.a],
-            b = this.vertices[face.b],
-            c = this.vertices[face.c],
-            d = this.vertices[face.d];
+    // all 4 faces in 2D, in the order RBLT
+    var faces2D = [this.faces[3], this.faces[0], this.faces[1], this.faces[2]].map(this.faceTo2D.bind(this));
 
-        //define local 3D coordinate system
-        var x3 = d.clone().sub(a).normalize();
-        var z3 = x3.clone().cross( b.clone().sub(a) ).normalize();
-        var y3 = z3.clone().cross( x3 );
+    var face0 = faces2D[0];
+    var face1 = this.faceToTrapezoid(faces2D[1], face0);
+    var face2 = this.faceToTrapezoid(faces2D[2], face1);
+    var face3 = this.faceToTrapezoid(faces2D[3], face2);
 
-        // Return a specific point in the Face4 into its 2D location
-        function pointToLocal2D(point) {
-            function projectScalar(a, b) {
-                return a.dot(b) / b.length();
-            }
-
-            var p = point.clone().sub(a);
-            var xValue = projectScalar(p, x3),
-                yValue = projectScalar(p, y3);
-            return new THREE.Vector2(xValue, yValue);
-        }
-
-        var pointsLocal = [face.a, face.b, face.c, face.d].map(function (idx) {
-                              return this.vertices[idx];
-                          }.bind(this)).map(pointToLocal2D);
-
-        return pointsLocal;
-    }.bind(this);
-
-    // all 4 faces in 2D version
-    var local2DFaces = this.faces.map(faceInLocal2D);
-
-    //return face2DPoints transformed to line up the edge with the end of previousPoints
-    function localPoints2DToTrapezoid(face2DPoints, previousPoints) {
-        function to3D(v2) {
-            return new THREE.Vector3(v2.x, v2.y, 0);
-        }
-
-        var oldDC = previousPoints[2].clone().sub(previousPoints[3]);
-        var origin = previousPoints[3];
-
-        var newAB = face2DPoints[1].clone().sub(face2DPoints[0]);
-        var rotAngle = to3D(oldDC).angleTo(to3D(newAB));
-
-        var transform = new THREE.Matrix4().makeFromPositionEulerScale( to3D(origin), new THREE.Vector3(0, 0, rotAngle), "XYZ", new THREE.Vector3(1,1,1) );
-
-        return face2DPoints.map(function (pt) {
-            var txPt3 = to3D(pt);
-            txPt3.applyMatrix4(transform);
-
-            return new THREE.Vector2(txPt3.x, txPt3.y);
-        });
-    }
-
-    var face0 = local2DFaces[0];
-    var face1 = localPoints2DToTrapezoid(local2DFaces[1], face0);
-    var face2 = localPoints2DToTrapezoid(local2DFaces[2], face1);
-    var face3 = localPoints2DToTrapezoid(local2DFaces[3], face2);
-
-    return [
+    //fullUnrolled is pyramidal
+    var fullUnrolled =
+           [
                face0[0], face0[1],
                face1[0], face1[1],
                face2[0], face2[1],
                face3[0], face3[1],
-               face3[3], face3[2]      //We have to add the last points in this order
+               face3[3], face3[2]      //We have to add the last points in the reverse order since that's how they're stored
            ];
+
+
+    //get a fan-like version by flipping the y axis and translating everything up
+    //will also have to re-order the points
+    
+
+    var fanlikeUnrolled = (function () {
+        var bound = new THREE.Box2().setFromPoints(fullUnrolled);
+
+        //TODO implement rotating/moving
+
+        return fullUnrolled;
+    })();
+
+    return fanlikeUnrolled;
 }
+
+THREE.Vector2.prototype.toString = function() { return "" + this.x.toFixed(5) + ',' + this.y.toFixed(5); };
+function ppPoints(trap) {
+    return trap.map(function (pt) { return pt.toString(); }).join("\n");
+}
+
+function ppPointsArr(traps) {
+    return traps.map(ppPoints).join("\n\n");
+}
+
+function len01(trap) { return trap[0].clone().sub(trap[1]).length(); }
+function len23(trap) { return trap[2].clone().sub(trap[3]).length(); }
