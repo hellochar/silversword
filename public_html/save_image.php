07e697e189742bd69928bf7fb2ed1a90ffb2b616
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-09-02 19:49:26 -0700

    fix save_image.php

--- public_html/save_image.php
+++ public_html/save_image.php
@@ -7,11 +7,12 @@ $imageData = str_replace(' ','+', $imageData);
 
 $image = base64_decode($imageData);
 
-function msTimeStamp() {
-    return round(microtime(true) * 1000);
-}
+$k = explode(" ", microtime());
+$secondsPart = intval($k[1] * 1000);
+$microsecondsPart = intval(round($k[0]*1000));
 
-$timestamp = msTimeStamp();
+$timestamp = $secondsPart + $microsecondsPart;
+
 $imageName = "images" . DIRECTORY_SEPARATOR . "lampdesignimages" . DIRECTORY_SEPARATOR . "$timestamp.png";
 
 file_put_contents($imageName, $image);
